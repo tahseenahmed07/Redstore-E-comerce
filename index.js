eeae20fc0c945949bb375d43f5fbf7ce8d45d6bd
Tahseen Ahmed <71816618+tahseenahmed07@users.noreply.github.com>
2021-10-25 23:28:27 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+// NAV MENU
+
+let menu_items = document.getElementById("menu-items");
+
+menu_items.style.maxHeight = "0px";
+
+function MenuToggle(){
+    if
+    (menu_items.style.maxHeight == "0px")
+    {
+    
+        menu_items.style.maxHeight = "700px";
+       
+
+    }
+    else
+    {menu_items.style.maxHeight = "0px"}
+        
+    
+}
